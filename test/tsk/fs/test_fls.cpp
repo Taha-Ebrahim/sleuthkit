f9ad03c2eb59a7a1521eb8e96e47860dc587a2f9
Pratik Singh <pratiksingh2k06@gmail.com>
2025-06-02 12:07:22 +0530

    Fixed memory leaks  in test_fls.cpp

--- test/tsk/fs/test_fls.cpp
+++ test/tsk/fs/test_fls.cpp
@@ -3,65 +3,61 @@
 #include "tsk/fs/tsk_fs.h"
 #include <cstring>
 #include <string>
+#include <memory>
 
-//Only one non-static function in tsk/fs/fls_lib.cpp 
+//Only one non-static function in tsk/fs/fls_lib.cpp
 
-// Helper function fr opening fs from image
-static TSK_FS_INFO* open_test_fs() {
-    const char *img_path = "test/data/image/image.dd";
-    TSK_IMG_INFO *img = tsk_img_open_sing(img_path, TSK_IMG_TYPE_DETECT, 0);
-    if (!img) {
-        WARN("Could not open test image: " << img_path << ". Skipping test.");
-        return nullptr;
+class FlsTestFS {
+public:
+    FlsTestFS(const TSK_TCHAR* img_path) {
+        img = tsk_img_open_sing(img_path, TSK_IMG_TYPE_DETECT, 0);
+        if (!img) return;
+        fs = tsk_fs_open_img(img, 0, TSK_FS_TYPE_DETECT);
     }
-    TSK_FS_INFO *fs = tsk_fs_open_img(img, 0, TSK_FS_TYPE_DETECT);
-    if (!fs) {
-        tsk_img_close(img);
-        WARN("Could not open filesystem in image: " << img_path << ". Skipping test.");
-        return nullptr;
+    ~FlsTestFS() {
+        if (fs) tsk_fs_close(fs);
+        if (img) tsk_img_close(img);
     }
-    return fs;
-}
+    TSK_FS_INFO* get() { return fs; }
+    bool valid() const { return img && fs; }
+private:
+    TSK_IMG_INFO* img = nullptr;
+    TSK_FS_INFO* fs = nullptr;
+};
 
 // Test tsk_fs_fls with a null prefix (tpre is nullptr)
 TEST_CASE("tsk_fs_fls_null_tpre", "[fls]") {
-    TSK_FS_INFO *fs = open_test_fs();
-    if (!fs) {
+    FlsTestFS testfs(_TSK_T("test/data/image/image.dd"));
+    if (!testfs.valid()) {
+        WARN("Could not open test image. Skipping test.");
         return;
     }
-    
-    uint8_t result = tsk_fs_fls(fs, (TSK_FS_FLS_FLAG_ENUM)0, 2, TSK_FS_DIR_WALK_FLAG_ALLOC, nullptr, 0);
+    uint8_t result = tsk_fs_fls(testfs.get(), (TSK_FS_FLS_FLAG_ENUM)0, 2, TSK_FS_DIR_WALK_FLAG_ALLOC, nullptr, 0);
     REQUIRE(result == 0);
-    
-    tsk_fs_close(fs);
 }
 
 // Test tsk_fs_fls with an empty string as prefix
 TEST_CASE("tsk_fs_fls_empty_tpre", "[fls]") {
-    TSK_FS_INFO *fs = open_test_fs();
-    if (!fs) {
+    FlsTestFS testfs(_TSK_T("test/data/image/image.dd"));
+    if (!testfs.valid()) {
+        WARN("Could not open test image. Skipping test.");
         return;
     }
-    
-    const char *empty = "";
-    uint8_t result = tsk_fs_fls(fs, (TSK_FS_FLS_FLAG_ENUM)0, 2, TSK_FS_DIR_WALK_FLAG_ALLOC, empty, 0);
+    const TSK_TCHAR *empty = _TSK_T("");
+    uint8_t result = tsk_fs_fls(testfs.get(), (TSK_FS_FLS_FLAG_ENUM)0, 2, TSK_FS_DIR_WALK_FLAG_ALLOC, empty, 0);
     REQUIRE(result == 0);
-    
-    tsk_fs_close(fs);
 }
 
 // Test tsk_fs_fls with a non-empty prefix string
 TEST_CASE("tsk_fs_fls_nonempty_tpre", "[fls]") {
-    TSK_FS_INFO *fs = open_test_fs();
-    if (!fs) {
+    FlsTestFS testfs(_TSK_T("test/data/image/image.dd"));
+    if (!testfs.valid()) {
+        WARN("Could not open test image. Skipping test.");
         return;
     }
-    
-    const char *pre = "prefix";
-    uint8_t result = tsk_fs_fls(fs, (TSK_FS_FLS_FLAG_ENUM)0, 2, TSK_FS_DIR_WALK_FLAG_ALLOC, pre, 0);
+    const TSK_TCHAR *pre = _TSK_T("prefix");
+    uint8_t result = tsk_fs_fls(testfs.get(), (TSK_FS_FLS_FLAG_ENUM)0, 2, TSK_FS_DIR_WALK_FLAG_ALLOC, pre, 0);
     REQUIRE(result == 0);
-    
-    tsk_fs_close(fs);
 }
 
 // Test tsk_fs_fls with disk image file and the root inode
